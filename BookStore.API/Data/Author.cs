1ba4820921a9fd1b8c63b0fdbb85c4a61026a0e3
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-08-10 14:41:13 +0300

    Added Author and Book classes to data

---
+++ BookStore.API/Data/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Data
+{
+    [Table("Authors")]
+    public partial class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+        public virtual IList<Book> Books { get; set; }
+    }
+}
---
+++ BookStore.API/Data/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Data
+{
+    [Table("Books")]
+    public partial class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int? Year { get; set; }
+        public string Isbn { get; set; }
+        public string Summary { get; set; }
+        public string Image { get; set; }
+        public decimal? Price { get; set; }
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+    }
+}
