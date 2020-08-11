3a2afb4a2a269cdce84cddb52025a90575d63322
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-08-11 15:01:05 +0300

    Added UserDTO and UsersController

---
+++ BookStore.API/Controllers/UsersController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.API.Contracts;
+using BookStore.API.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly ILoggerService logger;
+        public UsersController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, ILoggerService logger)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// User login end points
+        /// </summary>
+        /// <param name="userDTO"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] UserDTO userDTO)
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                var username = userDTO.Username;
+                var password = userDTO.Password;
+                logger.LogInfo($"{location}: Login attempt from user: {username}");
+                var result = await signInManager.PasswordSignInAsync(username, password, false, false);
+                if (result.Succeeded)
+                {
+                    logger.LogInfo($"{location}: {username} successfully authenticated");
+                    var user = await userManager.FindByNameAsync(username);
+                    return Ok(user.UserName);
+                }
+                logger.LogInfo($"{location}: {username} not authenticated");
+                return Unauthorized(userDTO.Username);
+            }
+            catch (Exception e)
+            {
+                return InternalError($"{location}: {e.Message} - {e.InnerException}");
+            }
+            
+        }
+
+        private string GetControllerActionNames()
+        {
+            var controller = ControllerContext.ActionDescriptor.ControllerName;
+            var action = ControllerContext.ActionDescriptor.ActionName;
+            return $"{controller} - {action}";
+        }
+        private ObjectResult InternalError(string message)
+        {
+            logger.LogError(message);
+            return StatusCode(500, "Something went wrong, please contact the Admin");
+        }
+    }
+}
