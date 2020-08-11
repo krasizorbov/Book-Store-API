a9ed075ab01dec33846e9740459e456d57d38db1
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-08-11 19:16:04 +0300

    Added Endpoints

---
+++ BookStore.UI/Static/Endpoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.UI.Static
+{
+    public class Endpoints
+    {
+        public static string BaseUrl = "http://localhost:44358/";
+        public static string AuthorsEndpoint = $"{BaseUrl}api/authors/";
+        public static string BooksEndpoint = $"{BaseUrl}api/books/";
+    }
+}
