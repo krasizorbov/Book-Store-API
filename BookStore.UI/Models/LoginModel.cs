60eb509f01b88921c1b8f4b5b7fb6af0b23b3922
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-08-12 04:19:06 +0300

    Refactoring

--- BookStore.UI/Models/LoginModel.cs
+++ BookStore.UI/Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace BookStore.UI.Models
 {
     public class LoginModel
     {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, ErrorMessage = "Your password is limited to {2} to {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
     }
 }
--- BookStore.UI/Static/Endpoints.cs
+++ BookStore.UI/Static/Endpoints.cs
@@ -10,6 +10,7 @@ namespace BookStore.UI.Static
         public static string BaseUrl = "https://localhost:44358/";
         public static string AuthorsEndpoint = $"{BaseUrl}api/authors/";
         public static string BooksEndpoint = $"{BaseUrl}api/books/";
-        public static string RegisterEndpoint = $"{BaseUrl}api/users/register";
+        public static string RegisterEndpoint = $"{BaseUrl}api/users/register/";
+        public static string LoginEndpoint = $"{BaseUrl}api/users/login/";
     }
 }
