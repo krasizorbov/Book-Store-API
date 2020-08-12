d25af6dbe89aa5995062ca57449df4294e672a8d
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-08-12 04:08:28 +0300

    Added IAuthenticationRepository, LoginModel, RegistrationModel, Register razor page and AuthenticationRepository

---
+++ BookStore.UI/Services/AuthenticationRepository.cs
@@ -0,0 +1,30 @@
+﻿using BookStore.UI.Contracts;
+using BookStore.UI.Models;
+using BookStore.UI.Static;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.UI.Services
+{
+    public class AuthenticationRepository : IAuthenticationRepository
+    {
+        private readonly IHttpClientFactory client;
+        public AuthenticationRepository(IHttpClientFactory client)
+        {
+            this.client = client;
+        }
+        public async Task<bool> Register(RegistrationModel user)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, Endpoints.RegisterEndpoint);
+            request.Content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+            var client = this.client.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
--- BookStore.UI/Startup.cs
+++ BookStore.UI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BookStore.UI.Data;
+using BookStore.UI.Contracts;
+using BookStore.UI.Services;
 
 namespace BookStore.UI
 {
@@ -28,7 +30,8 @@ namespace BookStore.UI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddHttpClient();
+            services.AddTransient<IAuthenticationRepository, AuthenticationRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
