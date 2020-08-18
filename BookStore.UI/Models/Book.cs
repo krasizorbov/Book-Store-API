4222f982c8350702c125768ea987c193ee5573fe
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-08-18 16:37:37 +0300

    Refactoring

--- BookStore.UI/Models/Book.cs
+++ BookStore.UI/Models/Book.cs
@@ -1,11 +1,15 @@
 ﻿namespace BookStore.UI.Models
 {
+    using System.ComponentModel.DataAnnotations;
     public class Book
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public int? Year { get; set; }
+        [Required]
         public string Isbn { get; set; }
+        [StringLength(250)]
         public string Summary { get; set; }
         public string Image { get; set; }
         public decimal? Price { get; set; }
